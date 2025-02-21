5400c53f03bde991549202cbfac656c1d4be9878
simonfotnt <simonpiar98@gamil.com>
2025-02-21 09:07:40 +0600

    variavle declear

---
+++ script.js
@@ -0,0 +1,6 @@
+const todoInput = document.querySelector("#todoInput");
+const addBtn = document.querySelector("#addButton");
+const todoList = document.querySelector("#todoList");     // storing html variable//
+
+
+document.addEventListener('DOMContentLoaded', loadTodos);
