4a3e40dce5b3002b1ada3ecb3ddd6e999bf30901
simonfotnt <simonpiar98@gamil.com>
2025-02-21 09:28:38 +0600

    created element to add in task like date li

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const todoList = document.querySelector("#todoList");     // storing html variab
 
 
 document.addEventListener('DOMContentLoaded', loadTodos);
+
+function addTodo(){
+    const task = todoInput.value.trim();
+    if(task === ""){
+        alert("Pleace enter Your todo");
+        return;
+    }
+
+    const date = new Date().toLocaleDateString(); // add date 
+
+    const li = document.createElement("li");
+    li.className = "flex items-center justify-between p-2 border rounded";
+    li.innerHTML = 
+    <span class="serial font-bold"></span> 
+    <span class="flex-grow task-text">${task}</span>                          // create elemnet
+    <span class="text-sm text-gray-500">${date}</span>
+    <div class="flex space-x-2">
+        <button class="editButton bg-yellow-300 hover:bg-yellow-400 p-1 rounded">Edit</button>
+        <button class="deleteButton bg-red-500 hover:bg-red-700 text-white p-1 rounded">Delete</button>
+    </div>
+;
+
+
+}
+
